4155cfc0ac40a74b37ead6d66b284c6b9f3184e1
Nazar8 <118745073+88n77@users.noreply.github.com>
2025-02-19 08:06:19 +0000

    upd

--- setup.sh
+++ setup.sh
@@ -4,34 +4,37 @@ GREEN='\e[38;5;46m'
 YELLOW='\e[38;5;226m'
 RESET='\e[0m'
 
-echo -e "${YELLOW}Оновлюємо сервер та встановлюємо необхідні компоненти...${RESET}"
+# Оновлення серверу та встановлення необхідних компонентів
+echo -e "${YELLOW}Оновлюємо сервер...${RESET}"
 sudo apt update && sudo apt upgrade -y
+
+echo -e "${YELLOW}Встановлюємо необхідні пакети...${RESET}"
 sudo apt install build-essential pkg-config libssl-dev git-all -y
 sudo apt install -y protobuf-compiler
-sudo apt install -y cargo
+sudo apt install cargo
 
-echo -e "${YELLOW}Завантажуємо та встановлюємо Rust...${RESET}"
+# Встановлення Rust
+echo -e "${YELLOW}Встановлюємо Rust...${RESET}"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 rustup update
-rustup self update
-rustup install stable
-source $HOME/.cargo/env
-rustc --version
 
+# Оновлення protobuf
+echo -e "${YELLOW}Оновлюємо protobuf...${RESET}"
+sudo apt remove -y protobuf-compiler
+curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v25.2/protoc-25.2-linux-x86_64.zip
+unzip protoc-25.2-linux-x86_64.zip -d $HOME/.local
+export PATH="$HOME/.local/bin:$PATH"
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+# Перевірка встановленої версії protobuf
+echo -e "${GREEN}Перевіряємо версію protobuf:${RESET}"
+protoc --version
 echo -e "${YELLOW}Встановлюємо screen для створення сесій...${RESET}"
 sudo apt install -y screen
 
-
-echo -e "${YELLOW}Запускаємо встановлення Nexus у фоновій сесії screen...${RESET}"
-screen -dmS nexus bash -c "
-    echo -e '${YELLOW}Завантажуємо та запускаємо скрипт Nexus...${RESET}';
-    curl -s https://cli.nexus.xyz/ | bash;
-    echo -e '${GREEN}Скрипт Nexus завершено.${RESET}';
-    exec bash
-"
-
-echo -e "${GREEN}Скрипт завершено! Нода Nexus запущена у фоновій сесії screen.${RESET}"
+echo -e "${GREEN}Скрипт завершено! Запустіть Nexus у фоновій сесії screen.${RESET}"
 echo -e "${YELLOW}Щоб підключитися до сесії, використовуйте команду:${RESET} screen -r nexus"
